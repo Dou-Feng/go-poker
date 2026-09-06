6d32eecf6ddac3c267176d15cb76d4a04470f383
walker.feng <821878167@qq.com>
2026-09-06 16:27:03 +0800

    remove competed module under /tmp

---
+++ tmp/allin_animation/README.md
@@ -0,0 +1,1 @@
+Open index.html. Click REPLAY ALL-IN to replay the burst animation.
--- tmp/new_seat/src/components/PlayerSeat/index.ts
+++
@@ -1,2 +0,0 @@
-export { default, PlayerSeat } from "./PlayerSeat";
-export type { PlayerSeatProps, PokerCard, SeatState, SeatPosition, Suit } from "./PlayerSeat.types";
